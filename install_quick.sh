481d774ef7bea55fa49553c992ea748e45721ca2
tau <tauwoo@seuxw.cc>
2018-06-18 08:57:17 +0000

    添加 .klei 配置项

--- install_quick.sh
+++ install_quick.sh
@@ -7,14 +7,17 @@ echo "开始安装 Don't Starve Together 独立服务器..."
 echo "                                        "
 echo "***************  食用说明  ***************"
 echo "                                        "
-echo "本安装方法为 steam 自带的安装方法，适合高阶用户"
-echo "运行完成后，需要手动修改 ~/.klei 中的配置文件"
+echo "本安装方法为 steam 快速的安装方法，适合开箱即玩"
 echo "                                        "
 echo "*****************************************"
 echo "                                        "
 
 echo "请输入您的token!"
 read TOKEN
+echo "请输入您的服务器名称!"
+read SERVER_NAME
+echo "请输入您的服务器密码!"
+read SERVER_PWD
 
 # 创建必要的目录
 echo "开始创建必要的目录..."
@@ -43,17 +46,14 @@ sleep 1
 echo "开始使用 steam_cmd 安装游戏..."
 STAEM_CMD_PATH=~/dst_data/steam_cmd/
 DST_SERVER_PATH=~/dst_data/dst_server/
+CLUSTER_PATH=~/.klei/DoNotStarveTogether/Cluster_1/
 
 cp $REPO_PATH/data/steamcmd_linux.tar.gz $STAEM_CMD_PATH
 cd $STAEM_CMD_PATH
 tar -xvzf steamcmd_linux.tar.gz
 ./steamcmd.sh +login anonymous +force_install_dir $DST_SERVER_PATH +app_update 343050 validate +quit
 
+# 配置 .klei
 apt-get install libcurl4-gnutls-dev:i386
-
-mkdir ~/.klei
-mkdir ~/.klei/DoNotStarveTogether
-mkdir ~/.klei/DoNotStarveTogether/Cluster_1
-echo $TOKEN > ~/.klei/DoNotStarveTogether/Cluster_1/cluster_token.txt
-
-$DST_SERVER_PATH/bin/dontstarve_dedicated_server_nullrenderer
+cp -r $REPO_PATH/data/.klei ~/
+echo $TOKEN > $CLUSTER_PATH/cluster_token.txt
